8e8737e1d7984ad3b0f63301ef08627a041b04b4
lehcode <53556648+lehcode@users.noreply.github.com>
2024-05-29 02:59:12 +0300

    feat: Redirect to full URL

--- src/routes/[shortUrl]/+server.ts
+++ src/routes/[shortUrl]/+server.ts
@@ -1,30 +1,34 @@
 import type { KVNamespace } from '@cloudflare/workers-types';
-import { getDefaultNS } from '$lib/kv/namespace';
+import { getDefaultNS, fetchUrlHash, getUrlDataByPrefix } from '$lib/kv/namespace';
 import { dev } from '$app/environment';
+import { ShortUrlLogEntry } from '../../app.d';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function GET({ getClientAddress, params, platform, request}) {
-  const { shortUrl } = params;
-  const kv:KVNamespace = getDefaultNS(dev, platform);
-  const list = await kv.list({ prefix: shortUrl });
-  const urlData = await kv.get<App.URLData>(list.keys[0].name, { type: 'json'});
+  let { shortUrl } = params;
+  const kv: KVNamespace = getDefaultNS(dev, platform);
+  const urlHash = await fetchUrlHash(shortUrl, kv);
+  // console.log(`URL hash for '${shortUrl}'`, urlHash);
   
-  console.log("urlData: ", urlData);
+  if (urlHash) {
+    const logEntry: ShortUrlLogEntry = {
+      createdAt: new Date().toISOString(),
+      userAgent: request.headers.get('user-agent'),
+      userIP: getClientAddress()
+    };
+    const urlSlug = `${urlHash}:${shortUrl}`;
+    const urlData = await getUrlDataByPrefix(kv, urlHash)
+    shortUrl = urlData.shortUrl;
 
-  if (!urlData) {
-    return new Response('URL not found', { status: 404 });
+    if (dev) {
+      console.log("logEntry", logEntry);
+      console.log("urlSlug", urlSlug);
+    }
+    
+    await kv.put(`${urlSlug}:logs`, JSON.stringify(logEntry), { metadata: { type: 'log' } });
+
+    return Response.redirect(urlData.longUrl, 302);
   }
 
-  const logEntry: App.ShortUrlLogEntry = {
-    createdAt: new Date().toISOString(),
-    userAgent: request.headers.get('user-agent'),
-    userIP: getClientAddress(),
-    userCountry: platform.cf.country,
-  };
-
-  console.log("logEntry", logEntry);
-  
-  await kv.put(`${shortUrl}:logs`, JSON.stringify(logEntry), { metadata: { type: 'log' } });
-
-  return Response.redirect(urlData.longUrl, 302);
+  return new Response('Not Found', { status: 404 });
 }
