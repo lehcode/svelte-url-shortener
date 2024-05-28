0278f9e7263d65f8e0632b489fd04bb1d7440037
lehcode <53556648+lehcode@users.noreply.github.com>
2024-05-29 02:56:38 +0300

    feat: Optimize worker code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
+import type { KVNamespace, R2Bucket } from '@cloudflare/workers-types';
 
 const cacheControl = {
   browserTTL: 259200, // 3 days (in seconds)
+};
+
+type Evt = {
+    request: Request;
+    waitUntil: (promise: Promise<unknown>) => void;
 };
 
 addEventListener("fetch", (event: any) => {
@@ -16,11 +22,6 @@ addEventListener("fetch", (event: any) => {
   event.respondWith(handleRequest(event));
 });
 
-type Evt = {
-    request: Request;
-    waitUntil: (promise: Promise<unknown>) => void;
-};
-
 async function handleRequest(fetchEvent: Evt) {
   const asset = await getAssetFromKV(fetchEvent)
 
@@ -31,4 +32,21 @@ async function handleRequest(fetchEvent: Evt) {
       "Cache-Control": `public, max-age=${cacheControl.browserTTL}`
     }
   });
+}
+
+export interface Env {
+	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+	MY_KV_NAMESPACE: KVNamespace;
+	//
+	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+	// MY_DURABLE_OBJECT: DurableObjectNamespace;
+	//
+	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+	MY_BUCKET: R2Bucket;
+	//
+	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
+	// MY_SERVICE: Fetcher;
+	//
+	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
+	// MY_QUEUE: Queue;
 }
