dd24698f856e69a6183d665ec0ba0c6ac947ae3a
lehcode <53556648+lehcode@users.noreply.github.com>
2024-05-28 08:30:18 +0300

    chore: Optimize dependencies and build

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig, loadEnv } from 'vite';
-// import vpc from "vite-plugin-cloudflare";
 
 export default defineConfig(({command, mode }) => {
 	const env = loadEnv(mode, process.cwd(), 'env');
 	const appEnv = JSON.stringify(env.APP_ENV);
 
 	const config = {
-		plugins: [
-			sveltekit(),
-			// vpc({
-			// 	scriptPath: "./src/index.ts",
-			// 	miniflare: {
-			// 		kvPersist: "./kv-data",
-			// 		host: "127.0.0.1",
-			// 		verbose: true,
-			// 	}
-			// })
-		],
+		plugins: [sveltekit()],
 		test: {
 			include: ['src/**/*.{test,spec}.{js,ts}']
 		},
